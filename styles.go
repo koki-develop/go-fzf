ef7a903a4fd4e8d6b7b6f2a3a4cfcc3afcc9908b
koki-develop <kou.pg.0131@gmail.com>
2023-03-24 09:35:15 +0900

    add styles

--- styles.go
+++ styles.go
@@ -16,10 +16,46 @@ type Style struct {
 	ForegroundColor string
 	BackgroundColor string
 	Bold            bool
+	Blink           bool
+	Italic          bool
+	Strikethrough   bool
+	Underline       bool
+	Faint           bool
 }
 
 type Styles struct {
 	option *stylesOption
+}
+
+func (s *Style) lipgloss() lipgloss.Style {
+	style := lipgloss.NewStyle()
+
+	if s.ForegroundColor != "" {
+		style = style.Foreground(lipgloss.Color(s.ForegroundColor))
+	}
+	if s.BackgroundColor != "" {
+		style = style.Background(lipgloss.Color(s.BackgroundColor))
+	}
+	if s.Bold {
+		style = style.Bold(true)
+	}
+	if s.Blink {
+		style = style.Blink(true)
+	}
+	if s.Italic {
+		style = style.Italic(true)
+	}
+	if s.Strikethrough {
+		style = style.Strikethrough(true)
+	}
+	if s.Underline {
+		style = style.Underline(true)
+	}
+	if s.Faint {
+		style = style.Faint(true)
+	}
+
+	return style
 }
 
 type StylesOption func(o *stylesOption)
@@ -68,20 +104,4 @@ func WithStyleMatches(s Style) StylesOption {
 	return func(o *stylesOption) {
 		o.matches = s.lipgloss()
 	}
-}
-
-func (s *Style) lipgloss() lipgloss.Style {
-	style := lipgloss.NewStyle()
-
-	if s.ForegroundColor != "" {
-		style = style.Foreground(lipgloss.Color(s.ForegroundColor))
-	}
-	if s.BackgroundColor != "" {
-		style = style.Background(lipgloss.Color(s.BackgroundColor))
-	}
-	if s.Bold {
-		style = style.Bold(true)
-	}
-
-	return style
 }

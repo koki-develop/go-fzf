c605667fb6987fbc5d212b6d30745a0ed7ac7137
koki-develop <kou.pg.0131@gmail.com>
2023-04-12 21:53:01 +0900

    fix border

--- option.go
+++ option.go
@@ -52,7 +52,7 @@ var defaultOption = option{
 		if borderw < 0 {
 			borderw = 0
 		}
-		_, _ = v.WriteString(strings.Repeat("─", borderw))
+		_, _ = v.WriteString(strings.Repeat("-", borderw))
 		return v.String()
 	},
 
